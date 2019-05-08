333ab3da697a36d02b0cf34b24ba9317c22f3db4
Pavel Kazakov <pavel_Kazakov@epam.com>
2019-05-08 18:21:09 +0300

    add delete experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -169,7 +169,7 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
-// @route  PODT api/profile/experience
+// @route  POST api/profile/experience
 // @desc   Add profile experience
 // @access Private
 router.post('/experience', [auth, [
@@ -212,5 +212,26 @@ router.post('/experience', [auth, [
     res.status(500).send('Server error');
   }
 }); 
+
+// @route  DELETE api/profile/experience/:exp_id
+// @desc   Delete experience from profile
+// @access Private
+router.delete('/experience/:exp_id', auth, async(req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
